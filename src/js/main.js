b36c1b46f49a33f70d62a9606936af999b83d79d
斯天明 <tianming.si@evenhidata.com>
2017-07-20 17:04:33 +0800

    添加oauth.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 // 主js文件
+import openid from  "../lib/oauth.min.js";   //前端方式获取用户的openid，需要时引入
 import "../css/main.css";
 import "./baidu.js";
 import "../lib/wechat-mobile-screen-adapt-640.js";
@@ -8,4 +9,5 @@ import $ from '../lib/third/jquery.min.js';
 // 主代码写在这里
 $(function(){
     console.log("main.js");
+    console.log("openid: ",openid);
 });
